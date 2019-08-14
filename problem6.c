c29541f36740cc6c15be1bd4496dd7bdf4f76bfd
Michael Schwarz <michael.schwarz93@gmail.com>
2019-08-14 10:59:30 +0200

    Add further problematic examples

---
+++ problem6.c
@@ -0,0 +1,24 @@
+int main(int argc, char const *argv[])
+{
+    int a[256];
+    int i = 0;
+    int zPend = 0;
+    int ll_i;
+    int nblock;
+
+    while (i < nblock)
+    { // TODO: Here
+        if (a[0] == ll_i) // this needs to be var == var for the problem to occur
+        {
+            zPend++;
+        }
+        else
+        {
+            a[0] = 8;
+        }
+        
+        i++;
+    }
+
+    return 0;  
+}
---
+++ problem7.c
@@ -0,0 +1,16 @@
+int main(int argc, char const *argv[])
+{
+    int i = 0;
+    int j;
+    int nGroups = 6;
+    int inUse16[16];
+
+    while (i < 16)
+    { // TODO: here
+        inUse16[i] = 0;
+        j = 0;
+        i++;
+    }
+
+    return 0;
+}
